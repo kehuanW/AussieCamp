36f4b5d79726c544c246570fb4e97719db5b8fe4
Kehuan <kehuanwang123@gmail.com>
2022-12-25 11:44:21 +1030

    fix card content and db

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/yelp-camp'
 
 mongoose.connect(dbUrl, {
     useUnifiedTopology: true
